7a176a4ee4fd651f9d8c5dddb2a60a7587d3be2b
Mohammed Umair <94831461+Mohammed-Umair@users.noreply.github.com>
2025-04-16 15:01:45 +0300

    updated

--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -32,30 +32,3 @@ export function Providers({ children }: { children: React.ReactNode }) {
 }
 
 
-// // app/provider.tsx
-// "use client";
-
-// import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-// import { Provider } from "react-redux";
-// import React, { useState } from "react";
-// import { store } from "@/redux/store";
-
-// export function Providers({ children }: { children: React.ReactNode }) {
-//   const [queryClient] = useState(
-//     () =>
-//       new QueryClient({
-//         defaultOptions: {
-//           queries: {
-//             staleTime: 5 * 70 * 1000,
-//             gcTime: 30 * 60 * 1000,
-//           },
-//         },
-//       })
-//   );
-
-//   return (
-//     <Provider store={store}>
-//       <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-//     </Provider>
-//   );
-// }
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -45,23 +45,3 @@ export type AppDispatch = typeof store.dispatch;
 export const useAppDispatch = () => useDispatch<AppDispatch>();
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 
-
-// // redux/store.ts
-// import { configureStore } from '@reduxjs/toolkit';
-// import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
-// import assessmentReducer from './slices/assessmentSlice';
-// import calendarReducer from './slices/calendarSlice';
-
-// export const store = configureStore({
-//   reducer: {
-//     assessments: assessmentReducer,
-//     calendar: calendarReducer,
-//   },
-// });
-
-// export type RootState = ReturnType<typeof store.getState>;
-// export type AppDispatch = typeof store.dispatch;
-
-// // Custom hooks for type safety
-// export const useAppDispatch = () => useDispatch<AppDispatch>();
-// export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
